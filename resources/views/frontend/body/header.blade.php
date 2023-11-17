f1d59fd4c5cf00beed998d8974eba23b33c908c5
David Adeniran <david.adeniran2016@gmail.com>
2023-11-17 18:33:48 +0100

    quick changes to fix previous deployment

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -266,7 +266,7 @@
                 <a href="{{ url('product/category/'.$category->id.'/'.$category->category_slug) }}">{{ $category->category_name }} <i class="fi-rs-angle-down"></i></a>
     
        @php 
-        $subcategories = App\Models\SubCategory::where('category_id',$category->id)->orderBy('subcategory_name','ASC')->get();
+        $subcategories = App\Models\Subcategory::where('category_id',$category->id)->orderBy('subcategory_name','ASC')->get();
             @endphp
     
                 <ul class="sub-menu">
